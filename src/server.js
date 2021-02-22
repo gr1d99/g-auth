55bac50d1eddd9e66fe77893d7dda8fbc9f3786a
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2021-02-22 13:03:56 +0300

    add tag

--- src/server.js
+++ src/server.js
@@ -25,6 +25,13 @@ server
         `<!doctype html>
     <html lang="">
     <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W3DS2M7');</script>
+<!-- End Google Tag Manager -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charset="utf-8" />
         <title>Welcome to Razzle</title>
@@ -42,6 +49,10 @@ server
         }
     </head>
     <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W3DS2M7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <div id="root">${markup}</div>
         <div id="g_id_onload"
              data-client_id="938366921015-gd6mtp2h7k6kqsn3nh77tolls2o66h0k.apps.googleusercontent.com"
